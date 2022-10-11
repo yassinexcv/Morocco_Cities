5ba8b5d13566a6b62d78480e78a7f2eff5ec1f3d
Y.TOUTI <yassinexcv188@gmail.com>
2022-10-11 15:31:19 +0000

    Update ville.sql

--- DataCities/SQL/ville.sql
+++ DataCities/SQL/ville.sql
@@ -3,7 +3,6 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Mer 28 Janvier 2015 à 23:12
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -11,16 +10,7 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
--- --------------------------------------------------------
-
---
--- Structure de la table `ville`
---
 
 CREATE TABLE IF NOT EXISTS `ville` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
