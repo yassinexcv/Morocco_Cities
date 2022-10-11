a687b86c1ed588b2404ed460a4bc150866a91f07
Y.touti <yassinexcv188@gmail.com>
2022-10-11 16:29:40 +0000

    Regions et villes du Maroc

---
+++ DataCities/SQL/region.sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Mer 28 Janvier 2015 à 23:12
+-- Version du serveur :  5.6.17
+-- Version de PHP :  5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `region`
+--
+
+CREATE TABLE IF NOT EXISTS `region` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `region` varchar(40) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
+
+--
+-- Contenu de la table `region`
+--
+
+INSERT INTO `region` (`id`, `region`) VALUES
+(1, 'Tanger-Tétouan-Al Hoceïma'),
+(2, 'l''Oriental'),
+(3, 'Fès-Meknès'),
+(4, 'Rabat-Salé-Kénitra'),
+(5, 'Béni Mellal-Khénifra'),
+(6, 'Casablanca-Settat'),
+(7, 'Marrakech-Safi'),
+(8, 'Drâa-Tafilalet'),
+(9, 'Souss-Massa'),
+(10, 'Guelmim-Oued Noun'),
+(11, 'Laâyoune-Sakia El Hamra'),
+(12, 'Dakhla-Oued Ed Dahab');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
